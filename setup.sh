6a60392d0ce8efa8b334c170240e6ce0a3eb46eb
skid901 <tlrudejr1993@naver.com>
2020-08-16 01:46:42 +0900

    python background

--- setup.sh
+++ setup.sh
@@ -4,6 +4,7 @@
 # python 버전 업그레이드: https://dlehdgml0480.tistory.com/8
 python -m venv .venv
 source .venv/bin/activate
-
 pip install -r requirements.txt
-python app.py
+
+# python 백그라운드 실행 및 종료: https://blkcoding.blogspot.com/2018/03/nohup.html
+nohup python app.py > log.txt &
